c5516a4ca77477e99f394c88a6d2d9964f378dcd
Nat Sharpe <sharpe1890@gmail.com>
2018-09-07 08:16:36 -0400

    added gifs of walkthrus and descriptions

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,38 @@
-gitHubButton = document.querySelector('.github');
-linkedInButton = document.querySelector('.linkedin');
-emailButton = document.querySelector('.email');
-phoneButton = document.querySelector('.phone');
-resumeButton = document.querySelector('.resume');
+let gitHubButton = document.querySelector('.github');
+let linkedInButton = document.querySelector('.linkedin');
+let emailButton = document.querySelector('.email');
+let phoneButton = document.querySelector('.phone');
+let callButton = document.querySelector('.call');
+let resumeButton = document.querySelector('.resume');
 
-silverGitHubButton = document.getElementById('.silver-github');
-silverDemoButton = document.querySelector('.silver-demo');
-shibaGitHubButton = document.getElementById('.shiba-github');
-shibaDemoButton = document.querySelector('.shiba-demo');
-vikingGitHubButton = document.getElementById('.viking-github');
-vikingDemoButton = document.querySelector('.viking-demo');
+let shiba = document.querySelector('.shiba');
+let shibaGif = document.querySelector('.shiba-gif')
+let silver = document.querySelector('.silver');
+let silverGif = document.querySelector('.silver-gif');
+let viking = document.querySelector('.viking');
+let vikingGif = document.querySelector('.viking-gif');;
+
+let toggleSilver = () => {
+    silverGif.classList.toggle('hidden')
+    silver.classList.toggle('hidden')
+};
+
+let toggleShiba = () => {
+    shibaGif.classList.toggle('hidden')
+    shiba.classList.toggle('hidden')
+};
+
+let toggleViking = () => {
+    vikingGif.classList.toggle('hidden')
+    viking.classList.toggle('hidden')
+};
+
+silver.addEventListener('click', toggleSilver);
+silverGif.addEventListener('click', toggleSilver);
+
+shiba.addEventListener('click', toggleShiba);
+shibaGif.addEventListener('click', toggleShiba);
+
+viking.addEventListener('click', toggleViking);
+vikingGif.addEventListener('click', toggleViking);
 
