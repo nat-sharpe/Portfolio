cc8380b50222ad8eb4a111fac77e33c3bf7af28c
Nat Sharpe <sharpe1890@gmail.com>
2018-09-07 05:20:28 -0400

    Project boxes nearly done

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+gitHubButton = document.querySelector('.github');
+linkedInButton = document.querySelector('.linkedin');
+emailButton = document.querySelector('.email');
+phoneButton = document.querySelector('.phone');
+resumeButton = document.querySelector('.resume');
+
+silverGitHubButton = document.getElementById('.silver-github');
+silverDemoButton = document.querySelector('.silver-demo');
+shibaGitHubButton = document.getElementById('.shiba-github');
+shibaDemoButton = document.querySelector('.shiba-demo');
+vikingGitHubButton = document.getElementById('.viking-github');
+vikingDemoButton = document.querySelector('.viking-demo');
+
